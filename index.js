3c91a15ccfe27dbbf021c86da6a9def472b5d41b
davicab <davicabral01@gmail.com>
2022-11-02 21:38:41 -0300

    fix

--- index.js
+++ index.js
@@ -163,22 +163,6 @@ function timer() {
     }, 1000);
 }
 
-const cursor = document.querySelector(".cursor");
-
-document.addEventListener("mousemove", (e) => {
-  let x = e.pageX;
-  let y = e.pageY;
-
-  cursor.style.top = y + "px";
-  cursor.style.left = x + "px";
-  cursor.style.display = "block";
-
-});
-
-document.addEventListener("mouseout", () => {
-  cursor.style.display = "none";
-});
-
 // function callStorage(){
 //     let tried = 0;
 //     MyValues.tries = tried
